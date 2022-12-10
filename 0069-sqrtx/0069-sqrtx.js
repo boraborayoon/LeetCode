da82c24b1217ca54e2432baa35b7a9c626e7d7d7
BoRa Yoon <89366567+boraborayoon@users.noreply.github.com>
2022-12-10 20:35:22 +0900

    Time: 129 ms (45.89%), Space: 44 MB (31.62%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(x**0.5)
+    let sqrt = 1;
+    if(x===0){
+        return 0;
+    }
+    for(let i=1; i*i<=x; i++){
+      sqrt = i;
+    }
+    return sqrt;
 };
