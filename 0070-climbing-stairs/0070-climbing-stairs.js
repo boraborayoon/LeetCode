95f2138ea97b807973444e71b113e5efedfd4b29
BoRa Yoon <89366567+boraborayoon@users.noreply.github.com>
2022-12-12 13:32:57 +0900

    Time: 90 ms (52.52%), Space: 42.1 MB (20.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if(n<4) return n
-    
-    let memo = [0,1,2,3]
-    
-    
-    for (let i = 4; i < n; i++) {
-        memo[i] = memo[i-1]+memo[i-2]
+    let result=[0,1,2]
+    for(let i=3;i<=n;i++){
+        result[i]=result[i-2]+result[i-1]
     }
     
-    return memo[n-1]+memo[n-2]
+    return result[n]
 };
+    
