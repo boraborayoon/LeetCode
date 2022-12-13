917d371e4cb7c24c47826bc4049bb744c6adf25f
BoRa Yoon <89366567+boraborayoon@users.noreply.github.com>
2022-12-13 11:36:59 +0900

    Time: 57 ms (100.00%), Space: 42.8 MB (83.00%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
@@ -3,26 +3,10 @@
  * @return {number}
  */
 var minFallingPathSum = function(matrix) {
-    let sum = Number.MAX_SAFE_INTEGER;
-    const dp = {};
-    
-    
-    function dfs(round,col){
-        const key = [round,col].join()
-        if(key in dp) return dp[key]
-        if(round === matrix.length) return 0
-        if(col >= matrix.length || col< 0) return Number.MAX_SAFE_INTEGER
-        
-        const res = matrix[round][col] + Math.min(dfs(round+1,col),dfs(round+1,col-1),dfs(round+1, col+1))
-        
-        dp[key]=res
-        
-        return res
+    for(let i=1;i<matrix.length;i++){
+        for(let j=0;j<matrix.length;j++){
+            matrix[i][j] += Math.min(j>0? matrix[i-1][j-1]:Infinity, matrix[i-1][j],j<matrix.length-1 ? matrix[i-1][j+1]:Infinity)
+        }
     }
-    
-    for(let i=0;i<matrix.length;i++){
-        sum = Math.min(sum, dfs(0,i))
-    }
-    
-    return sum
+    return Math.min(...matrix[matrix.length-1])
 };
