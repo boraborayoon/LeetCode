d330f100af33812d3c7706d7f23a9c56383663a7
BoRa Yoon <89366567+boraborayoon@users.noreply.github.com>
2022-12-17 14:27:56 +0900

    Time: 86 ms (81.95%), Space: 46.3 MB (23.71%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -3,35 +3,32 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-  let stack = [];
+    let stack = [];
     
     for(let i=0;i<tokens.length;i++){
-        if(Number(tokens[i]) || tokens[i]==='0'){
+        if(tokens[i]==='+'){
+            let num1=+stack.pop()
+            let num2=+stack.pop()
+            stack.push(num1+num2)
+        }
+        else if(tokens[i]==='-'){
+            let num1=+stack.pop()
+            let num2=+stack.pop()
+            stack.push(num2-num1)
+        }
+        else if(tokens[i]==='*'){
+            let num1=+stack.pop()
+            let num2=+stack.pop()
+            stack.push(num1*num2)
+        }
+        else if(tokens[i]==='/'){
+            let num1=+stack.pop()
+            let num2=+stack.pop()
+            stack.push(Math.trunc(num2/num1))
+        }
+        else{
             stack.push(tokens[i])
-        }else{
-            let num1=Number(stack.pop())
-            let num2=Number(stack.pop())
-            if(tokens[i]==='+'){
-                stack.push(String(num1+num2))
-            }
-            else if(tokens[i]==='-'){
-                stack.push(String(num2-num1))
-            }
-            else if(tokens[i]==='*'){
-                stack.push(String(num1*num2))
-            }
-            else{
-                if(num1===0 || num2===0){
-                    stack.push("0")
-                }
-                else if(num2/num1>0){
-                    stack.push(String(Math.floor(num2/num1)))
-                }
-                else{
-                    stack.push(String(Math.ceil(num2/num1)))
-                }
-            }
         }
     }
-    return Number(stack[0])
+    return stack[0]
 };
