caf63967d340b754f3d0695b64d6b3559fa818d4
BoRa Yoon <89366567+boraborayoon@users.noreply.github.com>
2023-01-05 19:53:26 +0900

    Time: 479 ms (22.07%), Space: 75.6 MB (10.34%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -3,15 +3,22 @@
  * @return {number}
  */
 var findMinArrowShots = function(points) {
-    points.sort((a,b)=>a[1]-b[1])
-    let arrows=1;
-    let end=points[0][1]
+    const n = points.length
+    let count = 1
     
-    for(let ballon of points){
-        if(ballon[0]>end){
-            end=ballon[1]
-            arrows++
+    if(n===0) return 0
+    if(n===1) return 1
+    
+    points.sort((a,b)=>a[1]-b[1])
+    
+    let pointer=0;
+    for(let i=1;i<n;i++){
+        if(points[pointer][1]>=points[i][0]) continue
+        else{
+            pointer=i
+            count++
         }
     }
-    return arrows
+    
+    return count
 };
